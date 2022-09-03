5ebb4a37bd7439a14277e318052cec6790f00901
Vasu-xD <81254651+Vasu-xD@users.noreply.github.com>
2022-09-03 16:22:53 +0530

    Update mongo.go

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -1,10 +1,8 @@
 
 import "go.mongodb.org/mongo-driver/mongo"
 
-serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
 clientOptions := options.Client().
-    ApplyURI("mongodb+srv://vasu:vasu@cluster0.3gnlkhi.mongodb.net/?retryWrites=true&w=majority").
-    SetServerAPIOptions(serverAPIOptions)
+    ApplyURI("mongodb+srv://vasu:vasu@cluster0.3gnlkhi.mongodb.net/?retryWrites=true&w=majority")
 ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 defer cancel()
 client, err := mongo.Connect(ctx, clientOptions)
