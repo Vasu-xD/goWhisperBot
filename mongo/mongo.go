2fe881e2dce4bd3f98ed06089d8c11b19d84e3ad
Vasu-xD <81254651+Vasu-xD@users.noreply.github.com>
2022-09-03 16:29:01 +0530

    Update mongo.go

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -1,11 +1,44 @@
+package mongo
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
 
-clientOptions := options.Client().
-    ApplyURI("mongodb+srv://vasu:vasu@cluster0.3gnlkhi.mongodb.net/?retryWrites=true&w=majority")
-ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-defer cancel()
-client, err := mongo.Connect(ctx, clientOptions)
-if err != nil {
-    log.Fatal(err)
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	Client *mongo.Client
+	Ctx    context.Context
+	err    error
+)
+
+func GetClient() *mongo.Client {
+	if Client != nil {
+		return Client
+	}
+
+	Client, err = mongo.NewClient(options.Client().ApplyURI(os.Getenv("DB_URI")))
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	Ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+	err = Client.Connect(Ctx)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return Client
+}
+
+func GetDatabase() *mongo.Database {
+	return GetClient().Database("whisper_bot")
 }
